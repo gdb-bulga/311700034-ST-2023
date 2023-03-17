c624b7b3af5b3936ff814ba25236fc8b2085982a
gdb-bulga <bulga0202@gmail.com>
2023-03-17 20:20:02 +0800

    Modify Lab04

--- Lab04/app.py
+++ Lab04/app.py
@@ -19,7 +19,7 @@ wait = WebDriverWait(driver, 60)
 driver.get("https://www.nycu.edu.tw/")
 
 wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'a[href = "https://www.nycu.edu.tw/news-network/"]'))).click()
-wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'div.eael-tabs-content ul[class="su-posts su-posts-list-loop"] li')))[0].click()
+wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'div.eael-tabs-content ul[class="su-posts su-posts-list-loop"] li a')))[0].click()
 
 print(wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'div[id="content-wrap"] header h1'))).get_attribute('innerHTML'))
 p = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'div[id="content-wrap"] div[class = "entry-content clr"] p')))
