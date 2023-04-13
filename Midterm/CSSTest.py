8e7dad4ed2bb0ac37444e96890b0dcd0be22742a
gdb-bulga <bulga0202@gmail.com>
2023-04-13 14:56:25 +0800

    Create Miterm

---
+++ Midterm/CSSTest.py
@@ -0,0 +1,67 @@
+import unittest
+from unittest.mock import patch
+from course_scheduling_system import CSS
+
+class TestCSS(unittest.TestCase):
+
+    def setUp(self):
+        self.css = CSS()
+
+    def test_q1_1(self):
+        course = ('Math', 'Monday', 2, 4)
+        with patch.object(CSS, 'check_course_exist', return_value=True):
+            result = self.css.add_course(course)
+            self.assertTrue(result)
+            self.assertIn(course, self.css.get_course_list())
+
+    def test_q1_2(self):
+        course1 = ('Math', 'Monday', 2, 4)
+        course2 = ('Physics', 'Monday', 3, 5)
+        with patch.object(CSS, 'check_course_exist', return_value=True):
+            self.css.add_course(course1)
+            result = self.css.add_course(course2)
+            self.assertFalse(result)
+            self.assertNotIn(course2, self.css.get_course_list())
+
+    def test_q1_3(self):
+        course = ('Math', 'Monday', 2, 4)
+        with patch.object(CSS, 'check_course_exist', return_value=False):
+            result = self.css.add_course(course)
+            self.assertFalse(result)
+            self.assertNotIn(course, self.css.get_course_list())
+
+    def test_q1_4(self):
+        course = 'Math'
+        with patch.object(CSS, 'check_course_exist', return_value=True):
+            with self.assertRaises(TypeError):
+                self.css.add_course(course)
+
+    def test_q1_5(self):
+        course1 = ('Math', 'Monday', 2, 4)
+        course2 = ('Physics', 'Tuesday', 3, 5)
+        course3 = ('Chemistry', 'Wednesday', 5, 7)
+        with patch.object(CSS, 'check_course_exist', return_value=True):
+            self.css.add_course(course1)
+            self.css.add_course(course2)
+            self.css.add_course(course3)
+            result = self.css.remove_course(course2)
+            self.assertTrue(result)
+            self.assertNotIn(course2, self.css.get_course_list())
+            self.assertEqual(4, self.css.check_course_exist.call_count)
+            expected_str = 'Monday    Tuesday   Wednesday Thursday  Friday    \n\
+|         |         |         |         |         \n\
+Math      |         |         |         |         \n\
+Math      |         |         |         |         \n\
+Math      |         |         |         |         \n\
+|         |         Chemistry |         |         \n\
+|         |         Chemistry |         |         \n\
+|         |         Chemistry |         |         \n'
+            self.assertEqual(expected_str, str(self.css))
+            
+    def test_q1_6(self):
+        with patch.object(CSS, 'check_course_exist', return_value=True):
+            # Add more test cases here to achieve 100% code coverage
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Midterm/app.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('--disable-gpu')
+
+#driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install())
+wait = WebDriverWait(driver, 10)
+
+# 1
+driver.get("https://docs.python.org/3/tutorial/index.html")
+
+# 找到下拉選單元素
+language_select = Select(wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'li.switchers select'))))
+
+# 選擇 "zh-tw"
+language_select.select_by_value('zh-tw')
+
+# 等待下拉選單選擇完成
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'li.switchers select option[value="zh-tw"][selected="selected"]')))
+
+# 印接下來的文字
+print(wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'section[id="the-python-tutorial"] h1'))).text)
+print(wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'section[id="the-python-tutorial"] p'))).get_attribute('innerHTML'))
+
+
+# 2
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'div.inline-search input'))).send_keys('class', Keys.RETURN)  
+wait.until(lambda driver: len(driver.find_elements(By.CSS_SELECTOR, 'div[id="search-results"] ul li a')) >= 5)
+result = driver.find_elements(By.CSS_SELECTOR, 'div[id="search-results"] ul li a')
+for i in result[:5]:
+    print(i.text)
+
+
+driver.quit()
